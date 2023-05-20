6025417cd264e8d38b5925dab33e6af6c4fe0190
Reaperrr <134102264+LowLevelReaperrr@users.noreply.github.com>
2023-05-20 19:39:43 -0400

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def op_function(num1, num2, op):
     elif op == "-":
         print(num1 - num2)
     elif op == "*":
-        print("num1 * num2)
+        print(num1 * num2)
     elif op == "/":
         print( num1 / num2)
     elif op == "//":
